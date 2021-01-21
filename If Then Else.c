b616aba732f55b4fb375e1368ea6a141d5e5faf6
tux0112 <ironhidedjokam313@gmail.com>
2021-01-21 19:09:19 +0800

    If Then Else.c
    melakukan percabangan if else dalam bahasa C

---
+++ If Then Else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    
+    if(a>0){
+        printf("positif\n");
+    }
+    else if(a<0){
+        printf("negatif\n");
+    }
+    else
+    {
+        printf("nol\n");
+    }
+}
