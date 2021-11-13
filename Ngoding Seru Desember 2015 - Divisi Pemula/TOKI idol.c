0ffa7a32f97e89e4e1742de8d8287a5320ecacf7
tux0112 <43138394+mrasw@users.noreply.github.com>
2021-11-14 04:45:43 +0800

    Create TOKI idol.c

---
+++ Ngoding Seru Desember 2015 - Divisi Pemula/TOKI idol.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    int d;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    d=a+b+c;
+
+    if(d>200) printf("Lolos\n");
+    
+    else
+    {
+        printf("Tidak Lolos\n"); 
+    }
+}
