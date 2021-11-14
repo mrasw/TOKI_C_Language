f71b10428d659799b149ddf18f8e7507ee0d5690
tux0112 <43138394+mrasw@users.noreply.github.com>
2021-11-15 00:01:19 +0800

    Add files via upload

---
+++ Ngoding Seru Oktober 2015 - Divisi Pemula/pak dengklek mau ujan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(0<=a&&a<=100)
+    {
+        printf("YA\n");
+    }
+    else
+    {
+        printf("TIDAK\n");
+    }
+}
