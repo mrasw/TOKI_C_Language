cb82675c1a0b6dc7fbc31908b5d8e5d829fb00d5
tux0112 <43138394+mrasw@users.noreply.github.com>
2021-11-20 14:42:31 +0800

    Add files via upload

---
+++ balik daftar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=0;
+    int arr[101];
+
+    while(scanf("%d",&arr[n])!=EOF)
+    {
+        n++;
+        
+    }
+    while(n--)
+    {
+        printf("%d\n",arr[n]);
+    }
+}
